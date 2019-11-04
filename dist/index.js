1a785acc10488a013b653723f340522c95e4c59e
Kjetil Midtgarden Golid <kjetil.golid@gmail.com>
2019-11-04 09:49:37 +0100

    update dist/index.js to reflect changes

--- dist/index.js
+++ dist/index.js
@@ -3501,7 +3501,7 @@
     p.fill(c2);
     p.rect(x, y, dim, dim);
     p.fill(c1);
-    p.circle(x + center, y + center, center, center);
+    p.ellipse(x + center, y + center, center, center);
   };
 
   const northwest_quarter_circle = (p, x, y, dim, c1, c2) => {
